1c466ef5eb7ba70b351ff1787677e7ffa873a517
Andreas Borg <borga@uni-mainz.de>
2014-11-17 07:58:03 +0000

    Implementierung AddPatientToken begonnen

--- src/de/pseudonymisierung/mainzelliste/client/AddPatientToken.java
+++ src/de/pseudonymisierung/mainzelliste/client/AddPatientToken.java
@@ -1,19 +1,46 @@
 package de.pseudonymisierung.mainzelliste.client;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
+import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
 public class AddPatientToken extends Token {
 
-	private LinkedList<String> idTypes;
-	private URL callback;
+	private LinkedList<String> idTypes = new LinkedList<String>();
+	private URL callback = null;
+	private Map<String, String> fields = new HashMap<String, String>();
 
+	public AddPatientToken addIdType(String idType) {
+		idTypes.add(idType);
+		return this;
+	}
+	
+	public AddPatientToken callback(URL callback) {
+		this.callback = callback;
+		return this;
+	}
+	
 	@Override
 	public JSONObject toJSON() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			JSONObject token = new JSONObject();
+			token.put("type", "addPatient");
+			JSONObject data = new JSONObject();
+			data.put("callback", this.callback);
+			
+			if (this.idTypes != null) {
+				// TODO
+			}
+			
+			token.put("data", data);
+			return token;
+		} catch (JSONException e) {
+			throw new Error(e);
+		}
 	}
 
 }
