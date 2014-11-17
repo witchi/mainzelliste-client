4fd069ab5017041609208cd355d173bc47f8a123
Andreas Borg <borga@uni-mainz.de>
2014-11-17 10:54:03 +0000

    Implementierung AddPatientToken (zu PROB-225: Schnittstelle zum Anlegen von createPatient-Token)

--- src/de/pseudonymisierung/mainzelliste/client/AddPatientToken.java
+++ src/de/pseudonymisierung/mainzelliste/client/AddPatientToken.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
@@ -12,6 +13,7 @@ public class AddPatientToken extends Token {
 
 	private LinkedList<String> idTypes = new LinkedList<String>();
 	private URL callback = null;
+	private String redirect = null;
 	private Map<String, String> fields = new HashMap<String, String>();
 
 	public AddPatientToken addIdType(String idType) {
@@ -19,8 +21,18 @@ public class AddPatientToken extends Token {
 		return this;
 	}
 	
+	public AddPatientToken addField(String fieldName, String value) {
+		this.fields.put(fieldName, value);
+		return this;
+	}
+	
 	public AddPatientToken callback(URL callback) {
 		this.callback = callback;
+		return this;
+	}
+
+	public AddPatientToken redirect(String redirect) {
+		this.redirect = redirect;
 		return this;
 	}
 	
@@ -31,9 +43,14 @@ public class AddPatientToken extends Token {
 			token.put("type", "addPatient");
 			JSONObject data = new JSONObject();
 			data.put("callback", this.callback);
+			data.put("redirect", this.redirect);
 			
-			if (this.idTypes != null) {
-				// TODO
+			if (this.idTypes.size() > 0) {
+				JSONArray idTypes = new JSONArray();
+				for (String thisIdType : this.idTypes) {
+					idTypes.put(thisIdType);
+				}
+				data.put("idTypes", idTypes);
 			}
 			
 			token.put("data", data);
