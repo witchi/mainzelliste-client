033e12ac14a8652e2d711ba1fce3e5ba0df37a02
Andreas Borg <borga@uni-mainz.de>
2015-01-20 10:24:44 +0000

    Codereview (JavaDoc, Formatierung)

--- src/de/pseudonymisierung/mainzelliste/client/InvalidSessionException.java
+++ src/de/pseudonymisierung/mainzelliste/client/InvalidSessionException.java
@@ -33,6 +33,7 @@ package de.pseudonymisierung.mainzelliste.client;
  */
 public class InvalidSessionException extends Exception {
 
+	@SuppressWarnings("javadoc")
 	private static final long serialVersionUID = 1L;
 
 }
