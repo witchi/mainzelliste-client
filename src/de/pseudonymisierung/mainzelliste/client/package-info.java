9e4f26f45ad9c7081b1611855f0fbdccd8d986d9
Andreas Borg <borga@uni-mainz.de>
2015-02-05 14:32:19 +0000

    Copyright, Javadoc für Paket

---
+++ src/de/pseudonymisierung/mainzelliste/client/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Client library for easy access to a Mainzelliste instance.
+ * 
+ * Mainzelliste (see <a
+ * href="http://www.mainzelliste.de" target="_blank">www.mainzelliste.de</a>}) is a web-based
+ * patient list and pseudonymization service. It provides a RESTful interface
+ * for client application. This library handles the HTTP calls necessary for
+ * using Mainzelliste from a client application, such as an electronic data
+ * capture (EDC) system.
+ * 
+ */
+package de.pseudonymisierung.mainzelliste.client;
