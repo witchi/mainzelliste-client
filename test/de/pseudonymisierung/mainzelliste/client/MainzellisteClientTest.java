f3230082bf234be1d9e613923c6f2af90fad952e
Andreas Borg <borga@uni-mainz.de>
2015-02-05 16:05:20 +0000

    Code formatiert

--- test/de/pseudonymisierung/mainzelliste/client/MainzellisteClientTest.java
+++ test/de/pseudonymisierung/mainzelliste/client/MainzellisteClientTest.java
@@ -14,10 +14,10 @@ public class MainzellisteClientTest {
 	private final String mainzellisteApiKey = "mdatborg";
 	private MainzellisteConnection con;
 	private MainzellisteConnection con2;
-	private final String testIds[] = {"1", "4", "5", "6", "10"};
+	private final String testIds[] = { "1", "4", "5", "6", "10" };
 	private final String defaultIdType = "intid";
-	private final String defaultFields[] = {"vorname", "nachname"}; 
-	
+	private final String defaultFields[] = { "vorname", "nachname" };
+
 	@Before
 	public void setUp() throws Exception {
 		con = new MainzellisteConnection(mainzellisteURL, mainzellisteApiKey);
@@ -29,7 +29,8 @@ public class MainzellisteClientTest {
 	}
 
 	@Test
-	public void test() throws MainzellisteNetworkException, InvalidSessionException {
+	public void test() throws MainzellisteNetworkException,
+			InvalidSessionException {
 		// Create a session and some temp ids
 		Session s = con.createSession();
 		HashMap<String, ID> ids = new HashMap<String, ID>();
@@ -37,15 +38,19 @@ public class MainzellisteClientTest {
 			ID thisId = new ID(defaultIdType, thisIdString);
 			ids.put(s.getTempId(thisId), thisId);
 		}
-		
-		Session s2 = con2.readSession(s.getSessionId());		
-		assertEquals( "Restored session contains different temp ids", s.getTempIds(), s2.getTempIds());
-		
+
+		Session s2 = con2.readSession(s.getSessionId());
+		assertEquals("Restored session contains different temp ids",
+				s.getTempIds(), s2.getTempIds());
+
 		for (String tempId : s.getTempIds()) {
 			ID id = s.getId(tempId);
-			assertEquals("Got different id for same temp id", ids.get(tempId), id);
-			assertEquals("Got different id for same temp id in restored session", id, s2.getId(tempId));
-			
+			assertEquals("Got different id for same temp id", ids.get(tempId),
+					id);
+			assertEquals(
+					"Got different id for same temp id in restored session",
+					id, s2.getId(tempId));
+
 		}
 	}
 
